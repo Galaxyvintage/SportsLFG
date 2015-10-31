3bd067b86b79c228ec197efa42a9e867820f221c
Zheyang Li <CL@cls-macbook-pro.local>
2015-10-31 01:20:42 -0700

    Added some documentation and some attributes in profile class; Changed some variable types in profile class;

--- SportsLFG/Profile.swift
+++ SportsLFG/Profile.swift
@@ -1,36 +1,46 @@
 //
-//  Profile.swift
-//  SportsLFG
+// File  : Login.swift
+// Author: Aaron Cheung, Charles Li
+// Date  : Oct 30 2015
+// Description : 
 //
-//  Created by Aaron Cheung on 2015-10-30.
-//  Copyright (c) 2015 CMPT-GP03. All rights reserved.
-//
-//  Profile class handles user's personal profile
 
 import Foundation
 
-class Profile {
-    var name: str
-    var age: Int
-    var isMale: Bool // gender boolean
-    var userID: Int
+//TODO:
+//     1. implement the init function
+//     2. verify user information using kinvey api 
+//     3. if user information is correct, current view is push to the HomeView
+//     4. if user information is incorrect, an alert will be shown to users 
+
+class Profile 
+{
+  var userName: String
+  var userAge: UInt
+  var isMale : Bool // gender boolean
+  var userID: UInt
     
-    //var rating: Double
-    //var achievementPoints: Int
+  //var rating: Double
+  //var achievementPoints: Int
     
-    init(userID id: Int) {
-        self.id = id
-    }
+  init(id: UInt, gender : Bool, name :String, age : UInt) 
+  {
+    userID   = id
+    userName = name
+    userAge  = age
+    isMale   = gender
+  }
     
-    // Function to retrieve user account information based on given account number
-    // Returns true if successful
-    private func getInfo(id: Int) -> Bool {
-        // do stuff
-        // self.name = databasegetName()
-        // something like that????
-        return true
-    }
+  // Function to retrieve user account information based on given account number
+  // Returns true if successful
+  private func getInfo(id: Int) -> Bool 
+  {
+    // do stuff
+    // self.name = databasegetName()
+    // something like that????
+    return true
+  }
     
-    //private func databaseGetName(userID)
+  //private func databaseGetName(userID)
     
 }
