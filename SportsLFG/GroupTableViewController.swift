38fb4c5451878a0758a7fd10dec6accfc7dc1de6
IsaacQ <IsaacQ@node-1w7jr9qhp8k9q01fgk2yrz3ms.ipv6.telus.net>
2015-11-06 23:39:27 -0800

    Finished Table List View and all the things except 1) [groups] Array database loading  & 2) Cell Constrain

--- SportsLFG/GroupTableViewController.swift
+++ SportsLFG/GroupTableViewController.swift
@@ -20,10 +20,10 @@ class GroupTableViewController: UITableViewController {
     func loadSampleMeals() {
         
         // sampel self created groups in groups array
-        let G1 = Group(name: "Group1", dateCreated: "Created date 1", startTime  : "startTime1", startDate  : "startDate2", sport  : "sport1", maxSize : "11", address: "add1", city    : "city1", province: "P1")
-        let G2 = Group(name: "Group2", dateCreated: "Created date 2", startTime  : "startTime2", startDate  : "startDate2", sport  : "sport2", maxSize : "22", address: "add2", city    : "city2", province: "P2")
-        let G3 = Group(name: "Group3", dateCreated: "Created date 3", startTime  : "startTime3", startDate  : "startDate3", sport  : "sport3", maxSize : "33", address: "add3", city    : "city3", province: "P3")
-        let G4 = Group(name: "Group4", dateCreated: "Created date 4", startTime  : "startTime4", startDate  : "startDate4", sport  : "sport4", maxSize : "44", address: "add4", city    : "city4", province: "P4")
+        let G1 = Group(name: "Group1", dateCreated: "Created date 1", startTime  : "startTime1", startDate  : "startDate2", sport  : "bB", maxSize : "11", address: "add1", city    : "city1", province: "P1")
+        let G2 = Group(name: "Group2", dateCreated: "Created date 2", startTime  : "startTime2", startDate  : "startDate2", sport  : "fB", maxSize : "22", address: "add2", city    : "city2", province: "P2")
+        let G3 = Group(name: "Group3", dateCreated: "Created date 3", startTime  : "startTime3", startDate  : "startDate3", sport  : "pP", maxSize : "33", address: "add3", city    : "city3", province: "P3")
+        let G4 = Group(name: "Group4", dateCreated: "Created date 4", startTime  : "startTime4", startDate  : "startDate4", sport  : "R", maxSize : "44", address: "add4", city    : "city4", province: "P4")
         
         groups += [G1,G2,G3,G4]
         groups += [G1,G2,G3,G4]
@@ -32,14 +32,9 @@ class GroupTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
         // Load the sample data.
         loadSampleMeals()
-    }
-
-    func loadSampleGroups() {
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -50,24 +45,53 @@ class GroupTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return groups.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "GroupTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! GroupTableViewCell
+        // Fetches the appropriate meal for the data source layout.
+        let group = groups[indexPath.row]
+        
         // Configure the cell...
-
+        cell.NameLabel.text = group.name
+        cell.StartDateLabel.text = group.startDate
+        cell.StartTimeLabel.text = group.startTime
+        cell.CreateDateLabel.text = group.dateCreated
+        cell.ProvienceLabel.text = group.province
+        cell.CityLabel.text = group.city
+        cell.AddressLabel.text = group.address
+        cell.MaxNumberLabel.text = group.maxSize
+        //cell.SportTypeImageView.image using switch, need to further change
+        switch group.sport{
+        case "bB":
+            let photo1 = UIImage(named: "Basketball-50_blue")!
+            cell.SportTypeImageView.image = photo1
+        case "fB":
+            let photo1 = UIImage(named: "Football 2-50_blue")!
+            cell.SportTypeImageView.image = photo1
+        case "pP":
+            let photo1 = UIImage(named: "Ping Pong-50_blue")!
+            cell.SportTypeImageView.image = photo1
+        case "R":
+            let photo1 = UIImage(named: "Running-50_blue")!
+            cell.SportTypeImageView.image = photo1
+        default:
+            let photod = UIImage(named: "defaultPhoto")!
+            cell.SportTypeImageView.image = photod
+            
+        }
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
