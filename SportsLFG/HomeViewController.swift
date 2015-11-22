23798816a633668a34a555e1d82f63baba9044e0
IsaacQ <IsaacQ@node-1w7jr9qhp8k9pn8dvv8buxosr.ipv6.telus.net>
2015-11-22 00:43:28 -0800

    Storyboard suck I didn't change it but it shows some changes. And I finished the taking photo from library. But I cannot test if I CAN TAKING A NEW PHOTO. (You can only choose from library now). There is ONE PROBLEM which is after you choose the photo, it will appear in the image view container. BUT the WHOLE HOMEPAGEVIEW will be horrible. And when you try to refresh that page, the photo view will change back to the default image.(JUST TRY.) IDK how to fix it since i think it is because of the HOMEPAGEVIEW setting. JUST TRY AND ASK ME ANY QUESTION IF YOU HAVE. AND THE VARIABLE selectedImage UNDER func imagePickerController IS THE ADDRESS OF WHAT WAS SELECTED FROM THE LIBRARY.

--- SportsLFG/HomeViewController.swift
+++ SportsLFG/HomeViewController.swift
@@ -1,38 +1,67 @@
 //
 // File   : HomeViewController.swift
-// Author : Charles Li
+// Author : Charles Li, Isaac Qiao
 // Date created: Oct.13 2015
-// Date edited : Nov.03 2015
+// Date edited : Nov.22 2015
 // Description: This is responsible for the home view controller and further functions need to be added in version 2.0
 
 import Foundation
 
-class HomeViewController : UIViewController,UINavigationBarDelegate,UIBarPositioningDelegate
+class HomeViewController : UIViewController, UINavigationBarDelegate, UIBarPositioningDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
-  //MARK:Properties
-  @IBOutlet weak var navigationBar: UINavigationBar!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.navigationBar.delegate = self
-  }
-  
-  ////////////////////
-  //Delegate Methods//
-  ////////////////////
-  
-  /*UIBar*/
-  func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
-    return UIBarPosition.TopAttached
-  }
-  
-  
-  
-  //MARK:Actions
-  @IBAction func Logout(sender: UIButton) {
-    //let LoginControllerView=self.storyboard?.instantiateViewControllerWithIdentifier("LoginNavigationController")
-    KCSUser.activeUser().logout()
-    performSegueWithIdentifier("GoBackToLogin", sender: UIButton.self)    
-  }
-
+    //MARK:Properties
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var PhotoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.delegate = self
+    }
+    
+    ////////////////////
+    //Delegate Methods//
+    ////////////////////
+    
+    /*UIBar*/
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        return UIBarPosition.TopAttached
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        PhotoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //MARK:Actions
+    @IBAction func Logout(sender: UIButton) {
+        //let LoginControllerView=self.storyboard?.instantiateViewControllerWithIdentifier("LoginNavigationController")
+        KCSUser.activeUser().logout()
+        performSegueWithIdentifier("GoBackToLogin", sender: UIButton.self)
+    }
+    
+    @IBAction func selectImageFromPhotoLibrary(sender: AnyObject) {
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
 }
