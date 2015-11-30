c1f33734aa7eacecd17056b5344e2321d64ed3ad
Isaac Qiao <bqiao@asb9700-e05.csil.sfu.ca>
2015-11-30 04:22:07 -0800

    finished the groupmamber tableview. didn't load date from database. need to finished that part. didn't add constrain to storyboard. wondering if we could make this into a container inside the group view. beside the info of that group.

---
+++ SportsLFG/GroupMemberTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  GroupMemberTableViewCell.swift
+//  SportsLFG
+//
+//  Created by Isaac Qiao on 2015-11-30.
+//  Copyright © 2015 CMPT-GP03. All rights reserved.
+//
+
+import UIKit
+
+class GroupMemberTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
