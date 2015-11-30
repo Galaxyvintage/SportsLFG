8e2039d170b71e58867f4cc55472676d385e680d
CharlesL <zla83@sfu.ca>
2015-11-30 01:52:01 -0800

    Added different cells in the energy record view controller.

---
+++ SportsLFG/EnergyTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EnergyTableViewCell.swift
+//  SportsLFG
+//
+//  Created by CharlesL on 2015-11-29.
+//  Copyright © 2015 CMPT-GP03. All rights reserved.
+//
+
+import Foundation
+
+class EnergyTableViewCell : UITableViewCell
+{
+  
+  @IBOutlet weak var TitleLabel: UILabel!
+
+}
--- SportsLFG/EnergyTableViewController .swift
+++ SportsLFG/EnergyTableViewController .swift
@@ -12,8 +12,12 @@ import UIKit
 
 class EnergyTableViewController : UITableViewController
 {
-  let kAuthorizeHealthKitSection = 2
-  let kProfileSegueIdentifier    = "profileSegue"
+  
+  internal var selectedIndexPath:NSIndexPath? = nil
+  
+  let kAuthorizeHealthKitSection = 1
+  var sectionZero = ["Water","Gatorade"] 
+  let sectionOne  = ["Authorize HealthKit"]
   
   let healthManager : HealthManager = HealthManager()
   
@@ -25,7 +29,7 @@ class EnergyTableViewController : UITableViewController
       if authorized
       {
         print("HealthKit authorization received")
-         let alert = UIAlertController(
+        let alert = UIAlertController(
           title: "Hello",
           message: "This app has been authorized successfully. Please go to the health app and change the settings under the Sources tab if needed", 
           preferredStyle: UIAlertControllerStyle.Alert)
@@ -50,19 +54,208 @@ class EnergyTableViewController : UITableViewController
       }
     }  
   }
-    
+  
   // MARK: - TableView Delegate
   
-  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    switch(indexPath.section, indexPath.row)
-    {
-    case(0,0):
-      authorizeHealthKit()
-    default:break
-    }
-    self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+  override func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+    self.view.endEditing(true)
+  }
+
+     
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 2
+    
   }
   
   
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    switch section
+    {
+    case 0 : return sectionZero.count
+    case 1 : return sectionOne.count
+    default: return 0
+    }
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    var title : String?
+    var cellIdentifier : String
+    if(selectedIndexPath != nil)
+    {
+      switch (selectedIndexPath!.section, selectedIndexPath!.row) 
+      {
+        
+      case (0,0)://(section: 0, row : 1) 
+        cellIdentifier = "WaterCell"
+        let Cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! WaterTableViewCell
+        return Cell
+        
+      case(0,1)://TODO: Need to change this 
+        cellIdentifier = "WaterCell"
+        let Cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! WaterTableViewCell
+        return Cell
+      default: break
+      }
+      
+    }  
+    switch indexPath.section{
+      
+      // first section
+    case 0:  
+      title = sectionZero[indexPath.row]
+      let cellIdentifier = "TitleCell"
+      let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! EnergyTableViewCell
+      cell.TitleLabel.text = title    
+      
+      return cell
+      // seoncd section
+    default:  
+      let cellIdentifier = "HealthKitCell"
+      let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) 
+      return cell
+      
+    }
+  }
+  
+  
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) 
+  {
+    
+    print("did select....")
+    // in fact, was this very row selected,
+    // and the user is clicking to deselect it...
+    // if you don't want "click a selected row to deselect"
+    // then on't include this clause.
+    if(selectedIndexPath == indexPath)
+    {
+      //REMOVE
+      print("(user clicked on selected to deselect)")
+      selectedIndexPath = nil
+      var indexPathToRemove : NSIndexPath
+      
+      tableView.deselectRowAtIndexPath(indexPath, animated:false)
+      switch (indexPath.section, indexPath.row) 
+      {
+      case (0,0): 
+        self.sectionZero.removeAtIndex(1)  // update the data source    
+        indexPathToRemove = NSIndexPath(forRow: 1, inSection: 0)
+      case (0,1): 
+        self.sectionZero.removeAtIndex(2)  // update the data source    
+        indexPathToRemove = NSIndexPath(forRow: 2, inSection: 0) 
+        
+      default: return      
+      }
+      NSLog("removing")
+      print(indexPathToRemove)
+      self.tableView.beginUpdates()
+      self.tableView.deleteRowsAtIndexPaths(NSArray(object:indexPathToRemove) as! [NSIndexPath], withRowAnimation: UITableViewRowAnimation.Fade)
+      self.tableView.endUpdates()
+      return
+    }
+    
+  
+    let currentSection = indexPath.section
+    var currentRow = indexPath.row
+    
+    
+    //something else is selected 
+    if(selectedIndexPath != nil)
+    {
+      let selectedRow = selectedIndexPath!.row
+      var indexPathToRemove : NSIndexPath
+      if(currentSection == selectedIndexPath!.section &&
+        currentRow > selectedIndexPath!.row)
+      {
+        currentRow = currentRow - 1 //there is an extra row above the current selected 
+                                    //that is about to be removed 
+      }
+      
+      tableView.deselectRowAtIndexPath(selectedIndexPath!, animated:false)
+      
+      switch selectedIndexPath!.section
+      {
+      case 0 :
+        self.sectionZero.removeAtIndex(selectedRow + 1)
+        indexPathToRemove = NSIndexPath(forRow: selectedRow + 1, inSection: 0)
+        
+      default: return 
+      } 
+      
+      self.tableView.beginUpdates()
+      NSLog("removing")
+      self.tableView.deleteRowsAtIndexPaths(NSArray(object: indexPathToRemove) as! [NSIndexPath], withRowAnimation: UITableViewRowAnimation.Fade)
+      self.tableView.endUpdates()
+    }
+    
+    
+    // no previous selection
+    selectedIndexPath = NSIndexPath(forRow: currentRow, inSection: currentSection)
+    
+    var indexPathToInsert : NSIndexPath
+    
+    switch(currentSection, currentRow)
+    {
+    case(kAuthorizeHealthKitSection,0):
+      authorizeHealthKit()
+      self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+      
+      return
+      
+      //Water 
+    case(0,0):
+      self.sectionZero.insert("WaterDataInput", atIndex: 1)
+      indexPathToInsert = NSIndexPath(forRow: 1, inSection: 0)
+      print(self.sectionZero)
+    case(0,1):
+      self.sectionZero.insert("WaterDataInput", atIndex: 2)//TODO:Need to change the string
+      indexPathToInsert = NSIndexPath(forRow: 2, inSection: 0)
+      print(self.sectionZero)
+    default:return
+      
+    }
+    self.tableView.beginUpdates()
+    self.tableView.insertRowsAtIndexPaths(NSArray(object: indexPathToInsert) as! [NSIndexPath], withRowAnimation: UITableViewRowAnimation.Fade)
+    self.tableView.endUpdates()
+  }
+  
+  
+  override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    
+    //if any cell is selected  
+    if let selected = selectedIndexPath
+    {
+      switch(selected.section,selected.row)
+      {
+      case(0,0):
+        if(indexPath.section   == 0 && 
+           indexPath.row       == 1 &&
+           self.sectionZero[1] == "WaterDataInput")
+        {
+          return CGFloat(157)
+        }
+      case(0,1):  
+        if(indexPath.section   == 0 && 
+           indexPath.row       == 2 &&
+           self.sectionZero[2] == "WaterDataInput")//TODO:Update the string
+        {
+          return CGFloat(157)
+        }
+
+        
+      default : return CGFloat(48)  
+      }
+    }  
+    return CGFloat(48)
+  }
+  
+  
+  //MARK: Actions
+  
+  @IBAction func Back(sender: AnyObject) {
+    
+    self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+  }
   
 }
